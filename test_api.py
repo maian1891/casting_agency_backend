edfca43cbb33e9585bfa99cfa2827491670a6eca
Mai Thien An <maian1891@gmail.com>
2024-05-18 12:27:37 +0700

    update test

--- test_api.py
+++ test_api.py
@@ -1,10 +1,11 @@
 import os
 import unittest
 import json
+import requests
 from flask_sqlalchemy import SQLAlchemy
 
 from api import create_app
-from models import setup_db
+from models.models import setup_db
 
 
 class ApiTestCase(unittest.TestCase):
@@ -12,21 +13,45 @@ class ApiTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
         self.database_path = os.environ['DATABASE_TEST_PATH']
-        setup_db(self.app, self.database_path)
+        self.app = create_app(self.database_path)
+        self.client = self.app.test_client
+        # setup_db(self.app, self.database_path)
 
         # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
+        # with self.app.app_context():
+        #     self.db.init_app(self.app)
+        #     # create all tables
+        #     self.db.create_all()
     
     def tearDown(self):
         """Executed after reach test"""
         pass
+
+
+    testingUsers = {
+        'testingUser2@funnymail.com': 'BuQ3tUS3 :jbFAL',
+        'testingUser3w@funnymail.com': 'y(1726854(b(-KY'
+        }
+    
+    # def getUserToken(user_name):
+    # # client id and secret come from LogIn (Test Client)! which has password enabled under "Client > Advanced > Grant Types > Tick Password"
+    #     url = 'https://YOUR_AUTH0_DOMAIN/oauth/token' 
+    #     headers = {'content-type': 'application/json'}
+    #     password = testingUsers[user_name]
+    #     parameter = { "client_id":"Jfjrl12w55uqcJswWmMhSm5IG2Qov8w2e", 
+    #                 "client_secret": "3E5ZnqLFbPUppBLQiGDjB0H2GtXaLyaD26sdk2HmHrBXQaDYE453UCUoUHmt5nWWh",
+    #                 "audience": 'AUTH0_AUDIENCE',
+    #                 "grant_type": "password",
+    #                 "username": userName,
+    #                 "password": password, "scope": "openid" } 
+    #     # do the equivalent of a CURL request from https://auth0.com/docs/quickstart/backend/python/02-using#obtaining-an-access-token-for-testing
+    #     responseDICT = json.loads(requests.post(url, json=parameter, headers=headers).text)
+    #     return responseDICT['access_token']
+
+    # @memoize # memoize code from: https://stackoverflow.com/a/815160
+    # def getUserTokenHeaders(userName='testingUser2@funnymail.com'):
+    #     return { 'authorization': "Bearer " + getUserToken(userName)}     
 
     """
     Write at least one test for each test for successful operation and for expected errors.
@@ -41,10 +66,6 @@ class ApiTestCase(unittest.TestCase):
         self.assertEqual(data["actors"], {
             "1": "Science",
             "2": "Art",
-            "3": "Geography",
-            "4": "History",
-            "5": "Entertainment",
-            "6": "Sports"
         })
 
     def test_get_actors_fail(self):
